51c01b4e2ae8d448aa281d60349adb2a050d4b31
rain <1544016010@qq.com>
2020-04-30 00:30:59 +0800

    fix lab2 promblem
    1. shamir(t, n), get t shadow keys from n can
      get the key.
    2. construct fraction addtion

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,50 +1,73 @@
 import random
 import json
 
+def gcd(a, b):
+    '''求最大公因数
+    '''
+    if b==0:
+        return a
+    while a%b!=0:
+        r = a%b
+        a, b = b, r
+    return b
+
+def fraction_add(a1, b1, a2, b2):
+    '''两分数相加，返回最简化后的分数
+    '''
+    # a1/b1 + a2/b2
+    a = a1*b2 + a2*b1
+    b = b1*b2
+    g = gcd(a, b)
+    return a//g, b//g
+
 def generate_key(parameter_boundary):
     k = random.randint(-parameter_boundary, parameter_boundary)
     return k
 
 def generate_polynomial(shadow_size, key, parameter_boundary):
-    #y(x) = an-1*x^(n-1) + an-2*x^(n-2) + ... + a2*x^2 + a1*x + a0
+    '''生成t-1次多项式系数
+    '''
+    #y(x) = at-1*x^(t-1) + at-2*x^(t-2) + ... + a2*x^2 + a1*x + a0
+    #a0==key
     l = []
     for _ in range(shadow_size - 1):
         l.append(random.randint(-parameter_boundary, parameter_boundary))
     l.append(key)
     return l
 
-def generate_shadow_keys(polynomial, parameter_boundary):
-    #l1:x1, x2, x3, ..., xn-1, xn
-    #l2:y1, y2, y3, ..., yn-1, yn
-    l1 = []
-    l2 = []
-    length = len(polynomial)
+def generate_shadow_keys(polynomial, N, parameter_boundary):
+    '''取n个多项式上不同的点
+    '''
+    #x_list:x1, x2, x3, ..., xn-1, xn
+    #y_list:y1, y2, y3, ..., yn-1, yn
+    x_list = []
+    y_list = []
     #different x (the function of random.sample)
-    l1 = random.sample(range(1, parameter_boundary), length)
-    for i in range(length):
-        y = 0
-        for j in range(length):
-            y = y + ((l1[i] ** (length - j - 1)) * polynomial[j])
-        l2.append(y)
-    return l1, l2
+    x_list = random.sample(range(1, parameter_boundary), N)
+    for x in x_list:
+        # use 秦九韶's algorithm to caculate polynomial
+        y = polynomial[0]
+        for coef in polynomial[1:]:
+            y = y*x +coef
+        y_list.append(y)
+    return x_list, y_list
 
 #Lagrangian interpolation
 def recover_key(shadow_keys, shadow_values):
-    key = 0
     length = len(shadow_keys)
+    key_a, key_b = 0, 1
     for i in range(length):
-        mul = 1
+        a, b = shadow_values[i], 1
         for j in range(length):
-            if i != j:
-                mul = mul * (shadow_keys[j] / (shadow_keys[j] - shadow_keys[i]))
-            else:
-                mul = mul
-        mul = mul * shadow_values[i]
-        key = key + mul
-    return key
+            if j!=i:
+                a, b =-shadow_keys[j]*a, (shadow_keys[i]-shadow_keys[j])*b
+                g = gcd(a, b)
+                a, b = a//g, b//g
+        key_a, key_b = fraction_add(key_a, key_b, a, b)
+    return key_a//key_b
 
 if __name__ == "__main__":
-    parameter_boundary = 2**7
+    parameter_boundary = 2**31
 
     menu = '''
     Menu:
@@ -54,6 +77,7 @@ if __name__ == "__main__":
     4) verify the key
     5) quit
     '''
+
     print(menu)
     while 1:
         cmd = input('~$')
@@ -63,43 +87,42 @@ if __name__ == "__main__":
                 json.dump(key, fp)
             print('Save the key to key.txt')
         elif cmd=='2':
-            with open('key.txt', 'rb') as fp:
+            with open('key.txt', 'r') as fp:
                 key = json.load(fp)
-            shadow_size = int(input('Please input the number of shadow keys:'))
+            shadow_size = int(input('Please input the number of t(shadow keys):'))
+            N = int(input('Please input the n:'))
             polynomial = generate_polynomial(shadow_size, key, parameter_boundary)
             with open('polynomial.txt', 'w') as fp:
                 json.dump(polynomial, fp)
-            shadow_keys, shadow_values = generate_shadow_keys(polynomial, parameter_boundary)
+            shadow_keys, shadow_values = generate_shadow_keys(polynomial, N, parameter_boundary)
             with open('shadow_values.txt', 'w') as fp:
                 json.dump(shadow_values, fp)
             with open('shadow_keys.txt', 'w') as fp:
                 json.dump(shadow_keys, fp)
             print('The shadow keys are generated.')
         elif cmd=='3':
-            shadow_keys = []
-            shadow_values = []
-            with open('shadow_keys.txt', 'rb') as fp:
+            with open('shadow_keys.txt', 'r') as fp:
                 shadow_keys = json.load(fp)
-            with open('shadow_values.txt', 'rb') as fp:
+            with open('shadow_values.txt', 'r') as fp:
                 shadow_values = json.load(fp)
-            rkey = recover_key(shadow_keys, shadow_values)
+            with open('polynomial.txt', 'r') as fp:
+                polynomial = json.load(fp)
+            shadow_size = len(polynomial) # t
+            samp_shadow_keys = shadow_keys[:shadow_size]
+            samp_shadow_values = shadow_values[:shadow_size]
+
+            rkey = recover_key(samp_shadow_keys, samp_shadow_values)
             with open('recover_key.txt', 'w') as fp:
                 json.dump(rkey, fp)
             print('The key is recovered.')
         elif cmd=='4':
-            with open('key.txt', 'rb') as fp:
+            with open('key.txt', 'r') as fp:
                 key = json.load(fp)
-            with open('recover_key.txt', 'rb') as fp:
+            with open('recover_key.txt', 'r') as fp:
                 rkey = json.load(fp)
-            if key < 0:
-                if(rkey > 1.05*key and rkey < 0.95*key):
-                    print('Succeed.')
-                else:
-                    print('Recover failure.')
+            if rkey==key:
+                print('Succeed.')
             else:
-                if(rkey <1.05*key and rkey > 0.95*key):
-                    print('Succeed.')
-                else:
-                    print('Recover failure.')
+                print('Recover failure.')
         else:
             exit(0)
--- lab2/test.py
+++ lab2/test.py
@@ -12,37 +12,76 @@ np.set_printoptions(suppress=True)
 # coefficient_list =  [-27, 116, 105, 121, -21, -101, -14, -62]
 # x_list =            [53, 106, 36, 113, 119, 88, 80, 52]
 # y_list =            [-29101273020599, -3893833095574998, -1856774608838, -6108574139830619, -8792209469678249, -1048991876399694, -535473327239582, -25423742344998]
-coefficient_list =  [-11, -110, 84, -22, 17, 68, -109, -62]
-x_list =            [91, 100, 25, 17, 3, 19, 89, 37]
-y_list =            [-630380047300975, -1209162182330962, -93182116537, -7051326209, -84935, -14802371767, -540744932678777, -1320695942509]
+# coefficient_list =  [-11, -110, 84, -22, 17, 68, -109, -62]
+# x_list =            [91, 100, 25, 17, 3, 19, 89, 37]
+# y_list =            [-630380047300975, -1209162182330962, -93182116537, -7051326209, -84935, -14802371767, -540744932678777, -1320695942509]
 
 # coefficient_list =  [128, -120, 116, 4, -101, 72, -49, -72, -72, -62]
 # x_list =            [103, 102, 86, 108, 22, 20, 41, 10, 15, 54]
 # y_list =            [165505113582708815352, 151579185677466631354, 32582885013147162874, 253671361251755778082, 148234649879034, 62612423897698, 40969295569596964, 117154563018, 4632995140408, 491230290148038010]
+coefficient_list =  [90, 3, 33, -55, 62]
+x_list =            [73, 7, 89, 85, 77]
+y_list =            [2557180645, 218413, 5649173157, 4700132437, 3165334773]
+def polynomial(x):
+    # y = 0
+    # for i, coef in enumerate(coefficient_list):
+    #     y += coef*x**(N-1-i)
+    # return y
+    y = coefficient_list[0]
+    for coef in coefficient_list[1:]:
+        y = y*x +coef
+    return y
 
-N = len(coefficient_list)
-y2_list = []
-for x in x_list:
-    y = 0
-    for i, coef in enumerate(coefficient_list):
-        y += coef*x**(N-1-i)
-    y2_list.append(y)
+y2_list = list(map(polynomial, x_list))
 
-print(y_list == y2_list)
+print(y_list== y2_list)
 
-M = np.zeros((N, N+1), dtype=np.int64)
-'''
-[x1^t-1, x1^t-2, ..., 1, y_{i1}]
-[x2^t-1, x2^t-2, ..., 1, y_{i2}]
-:
-.
-[xt^t-1, xt^t-2, ..., 1, y_{it}]
-'''
-for i in range(N): 
-    for j in range(N):
-        M[i][j] = x_list[i]**(N-1-j)
-for i in range(N):
-    M[i][N] = y_list[i]
+# N = len(coefficient_list)
+# M = np.zeros((N, N+1), dtype=np.int64)
+# '''
+# [x1^t-1, x1^t-2, ..., 1, y_{i1}]
+# [x2^t-1, x2^t-2, ..., 1, y_{i2}]
+# :
+# .
+# [xt^t-1, xt^t-2, ..., 1, y_{it}]
+# '''
+# for i in range(N): 
+#     for j in range(N):
+#         M[i][j] = x_list[i]**(N-1-j)
+# for i in range(N):
+#     M[i][N] = y_list[i]
 
-print(M)
-print(np.linalg.matrix_rank(M))
+# print(M)
+# print(np.linalg.matrix_rank(M))
+
+def gcd(a, b):
+    if b==0:
+        return a
+    while a%b!=0:
+        r = a%b
+        a, b = b, r
+    return b
+
+def fraction_add(a1, b1, a2, b2):
+    # a1/b1 + a2/b2
+    a = a1*b2 + a2*b1
+    b = b1*b2
+    g = gcd(a, b)
+    return a//g, b//g
+
+#Lagrangian interpolation
+def recover_key(shadow_keys, shadow_values):
+    length = len(shadow_keys)
+    key_a, key_b = 0, 1
+    for i in range(length):
+        a, b = shadow_values[i], 1
+        for j in range(length):
+            if j!=i:
+                a, b =-shadow_keys[j]*a, (shadow_keys[i]-shadow_keys[j])*b
+                g = gcd(a, b)
+                a, b = a//g, b//g
+        key_a, key_b = fraction_add(key_a, key_b, a, b)
+    return key_a//key_b
+
+rkey = recover_key(x_list, y_list)
+print(rkey, coefficient_list[-1])
